c9a1cb98ac7232cd49471adafde78dcd69df3f72
Troy Landers <tlanders@landersconsulting.biz>
2021-02-15 23:16:53 -0600

    added next and previous click functions

--- src/AnimatedCard.js
+++ src/AnimatedCard.js
@@ -1,14 +1,17 @@
-export const AnimatedCard = () => {
+export const AnimatedCard = (props) => {
+    const cards = props.cards;
+    const cardIndex = props.cardIndex;
+    const theCard = cards[cardIndex];
+
     return (
         <div className="card slide-left">
             {/*<img src="..." className="card-img-top" alt="...">*/}
             <div className="card-body">
-                <h2 className="card-title card-back">Card title</h2>
-                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dicta ex provident
-                    quos reiciendis. Possimus!</p>
+                <h2 className="card-title card-back">{theCard.title}</h2>
+                <p className="card-text">{theCard.desc}</p>
                 <div className={"row"}>
-                    <a href="#" className="btn btn-primary col-4 m-2 mx-auto">Previous</a>
-                    <a href="#" className="btn btn-primary col-4 m-2 mx-auto">Next</a>
+                    <a href="#" className="btn btn-primary col-4 m-2 mx-auto" onClick={props.previousClick}>Previous</a>
+                    <a href="#" className="btn btn-primary col-4 m-2 mx-auto" onClick={props.nextClick}>Next</a>
                 </div>
             </div>
         </div>
--- src/AnimatedCards.js
+++ src/AnimatedCards.js
@@ -1,14 +1,35 @@
+import {useState} from 'react';
 import {AnimatedCard} from "./AnimatedCard";
 
-export const AnimatedCards = () => (
-    <>
-        <h1>Animated Cards</h1>
-        <div className={"container"}>
-            <div className={"row"}>
-                <div className={"col-6 mx-auto"}>
-                    <AnimatedCard/>
+export const AnimatedCards = () => {
+    const [cardIndex, setCardIndex] = useState(0);
+    const cards = [
+        {title: "Card 0", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing."},
+        {title: "Card 1", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum."},
+        {title: "Card 2", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing."},
+        {title: "Card 3", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum."},
+        {title: "Card 4", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing."},
+    ];
+
+    const nextCardClick = (index) => {
+        setCardIndex(index + 1);
+    }
+    const previousCardClick = (index) => {
+        setCardIndex(index - 1);
+    }
+
+    return (
+        <>
+            <h1>Animated Cards</h1>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className={"col-6 mx-auto"}>
+                        <AnimatedCard cards={cards} cardIndex={cardIndex}
+                                      previousClick={() => previousCardClick(cardIndex)}
+                                      nextClick={() => nextCardClick(cardIndex)}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
+}
