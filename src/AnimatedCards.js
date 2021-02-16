c42981036ad23815cd3f6e72f97960715a1ffabf
Troy Landers <tlanders@landersconsulting.biz>
2021-02-15 22:22:23 -0600

    initial commit

---
+++ src/AnimatedCards.js
@@ -0,0 +1,19 @@
+export const AnimatedCards = () => (
+    <>
+        <h1>Animated Cards</h1>
+        <div className={"container"}>
+            <div className={"row"}>
+                <div className={"col-6 mx-auto"}>
+                    <div className="card">
+                        {/*<img src="..." className="card-img-top" alt="...">*/}
+                        <div className="card-body">
+                            <h2 className="card-title">Card title</h2>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dicta ex provident quos reiciendis. Possimus!</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+);
