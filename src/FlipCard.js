5781655262239847e13d0d9df76efd724b042f6a
Troy Landers <tlanders@landersconsulting.biz>
2021-04-18 11:54:35 -0500

    upgraded npm added example translations

--- src/FlipCard.js
+++ src/FlipCard.js
@@ -4,6 +4,122 @@ export const FlipCard = (props) => {
     const {cards, cardIndex, isShowing, showText} = props;
     const theCard = cards[cardIndex];
 
+    theCard.example = 'Hola mi amigo'
+    theCard.exampleTranslation = 'Hello my friend'
+
+    let extraInfo = ''
+    if(showText) {
+        extraInfo = (
+            <div className={"text-focus-in"}>
+                <hr/>
+                <h6>Present Tense</h6>
+                <div className={"row"}>
+                    <div className={"col-6"}>
+                        <p>
+                            <span className={"text-muted"}>yo</span> trabajo<br/>
+                            <span className={"text-muted"}>tu</span> trabajas<br/>
+                            <span className={"text-muted"}>el</span> trabaja
+                        </p>
+                    </div>
+                    <div className={"col-6"}>
+                        <p>
+                            <span className={"text-muted"}>nosotros</span> trabajamos<br/>
+                            <span className={"text-muted"}>ellos / ustedes</span> trabajan
+                        </p>
+                    </div>
+                    <div className={"col-12"}>
+                        <p className="card-text">Trabajo mucho<br/>
+                            <span className={"font-italic"}>I work a lot</span>
+                        </p>
+                    </div>
+                </div>
+                <div className={"row p-2"}>
+                    <div className={"col-12"}>
+                        <div className="accordion" id="accordionExample">
+                            <div className="card">
+                                <div className="card-header" id="headingOne">
+                                    <h2 className="mb-0">
+                                        <button className="btn btn-link btn-block text-center" type="button"
+                                                data-toggle="collapse" data-target="#collapseOne"
+                                                aria-expanded="true" aria-controls="collapseOne">
+                                            Imperfect Tense
+                                        </button>
+                                    </h2>
+                                </div>
+
+                                <div id="collapseOne" className="collapse" aria-labelledby="headingOne"
+                                     data-parent="#accordionExample">
+                                    <div className="card-body">
+                                        <p className={"font-italic"}>o &rarr; ue</p>
+                                        <div className={"row"}>
+                                            <div className={"col-6"}>
+                                                <p>
+                                                    <span className={"text-muted"}>yo</span> trabajaba<br/>
+                                                    <span className={"text-muted"}>tu</span> trabajabas<br/>
+                                                    <span className={"text-muted"}>el</span> trabajaba
+                                                </p>
+                                            </div>
+                                            <div className={"col-6"}>
+                                                <p>
+                                                    <span className={"text-muted"}>nosotros</span> trabajamos<br/>
+                                                    <span className={"text-muted"}>ellos / ustedes</span> trabajaban
+                                                </p>
+                                            </div>
+                                            <div className={"col-12"}>
+                                                <p className="card-text">Trabajaba mucho<br/>
+                                                    <span className={"font-italic"}>I worked a lot</span>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={"row p-2"}>
+                    <div className={"col-12"}>
+                        <div className="accordion" id="accordion2">
+                            <div className="card">
+                                <div className="card-header" id="heading2">
+                                    <h2 className="mb-0">
+                                        <button className="btn btn-link btn-block text-center" type="button"
+                                                data-toggle="collapse" data-target="#collapse2"
+                                                aria-expanded="true" aria-controls="collapse2">
+                                            Preterite Tense
+                                        </button>
+                                    </h2>
+                                </div>
+
+                                <div id="collapse2" className="collapse" aria-labelledby="heading2"
+                                     data-parent="#accordion2">
+                                    <div className="card-body">
+                                        <p className={"font-italic"}>i &rarr; ie</p>
+                                        <div className={"row"}>
+                                            <div className={"col-6"}>
+                                                <p>
+                                                    <span className={"text-muted"}>yo</span> trabaje<br/>
+                                                    <span className={"text-muted"}>tu</span> trabajaste<br/>
+                                                    <span className={"text-muted"}>el</span> trabajo
+                                                </p>
+                                            </div>
+                                            <div className={"col-6"}>
+                                                <p>
+                                                    <span className={"text-muted"}>nosotros</span> trabajamos<br/>
+                                                    <span className={"text-muted"}>ellos / ustedes</span> trabajaron
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className={classnames('card',
             {'front-card swing-in-top-fwd' : isShowing},
@@ -11,139 +127,14 @@ export const FlipCard = (props) => {
             <div className="card-body">
                 <div className={"p-3 bg-light"}>
                     <h5 className="card-title card-back">{theCard.item}</h5>
-                    {showText && (<h6 className="card-text m-4 text-focus-in">{theCard.translation}</h6>)}
-                    {!showText && (<h6 className="card-text m-4">&nbsp;</h6>)}
-                    <hr/>
-                    <h6>Present Tense</h6>
-                    <div className={"row"}>
-                        <div className={"col-6"}>
-                            <p>
-                                <span className={"text-muted"}>yo</span> trabajo<br/>
-                                <span className={"text-muted"}>tu</span> trabajas<br/>
-                                <span className={"text-muted"}>el</span> trabaja
-                            </p>
-                        </div>
-                        <div className={"col-6"}>
-                            <p>
-                                <span className={"text-muted"}>nosotros</span> trabajamos<br/>
-                                <span className={"text-muted"}>ellos / ustedes</span> trabajan
-                            </p>
-                        </div>
-                    </div>
-                    <hr/>
-                    <h6>Preterite Tense</h6>
-                    <p className={"font-italic"}>i &rarr; ie</p>
-                    <div className={"row"}>
-                        <div className={"col-6"}>
-                            <p>
-                                <span className={"text-muted"}>yo</span> trabaje<br/>
-                                <span className={"text-muted"}>tu</span> trabajaste<br/>
-                                <span className={"text-muted"}>el</span> trabajo
-                            </p>
-                        </div>
-                        <div className={"col-6"}>
-                            <p>
-                                <span className={"text-muted"}>nosotros</span> trabajamos<br/>
-                                <span className={"text-muted"}>ellos / ustedes</span> trabajaron
-                            </p>
-                        </div>
-                    </div>
-                    <hr/>
-                    <h6>Imperfect Tense</h6>
-                    <p><span className={"text-muted font-italic"}>type:</span> o &rarr; ue</p>
-                    <div className={"row"}>
-                        <div className={"col-6"}>
-                            <p>
-                                <span className={"text-muted"}>yo</span> trabajaba<br/>
-                                <span className={"text-muted"}>tu</span> trabajabas<br/>
-                                <span className={"text-muted"}>el</span> trabajaba
-                            </p>
-                        </div>
-                        <div className={"col-6"}>
-                            <p>
-                                <span className={"text-muted"}>nosotros</span> trabajamos<br/>
-                                <span className={"text-muted"}>ellos / ustedes</span> trabajaban
-                            </p>
-                        </div>
-                    </div>
-                    <div className={"row"}>
-                        <div className={"col-12"}>
-                            <div className="accordion" id="accordionExample">
-                                <div className="card">
-                                    <div className="card-header" id="headingOne">
-                                        <h2 className="mb-0">
-                                            <button className="btn btn-link btn-block text-center" type="button"
-                                                    data-toggle="collapse" data-target="#collapseOne"
-                                                    aria-expanded="true" aria-controls="collapseOne">
-                                                Imperfect Tense
-                                            </button>
-                                        </h2>
-                                    </div>
-
-                                    <div id="collapseOne" className="collapse" aria-labelledby="headingOne"
-                                         data-parent="#accordionExample">
-                                        <div className="card-body">
-                                            <p className={"font-italic"}>o &rarr; ue</p>
-                                            <div className={"row"}>
-                                                <div className={"col-6"}>
-                                                    <p>
-                                                        <span className={"text-muted"}>yo</span> trabajaba<br/>
-                                                        <span className={"text-muted"}>tu</span> trabajabas<br/>
-                                                        <span className={"text-muted"}>el</span> trabajaba
-                                                    </p>
-                                                </div>
-                                                <div className={"col-6"}>
-                                                    <p>
-                                                        <span className={"text-muted"}>nosotros</span> trabajamos<br/>
-                                                        <span className={"text-muted"}>ellos / ustedes</span> trabajaban
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"row"}>
-                        <div className={"col-12"}>
-                            <div className="accordion" id="accordion2">
-                                <div className="card">
-                                    <div className="card-header" id="heading2">
-                                        <h2 className="mb-0">
-                                            <button className="btn btn-link btn-block text-center" type="button"
-                                                    data-toggle="collapse" data-target="#collapse2"
-                                                    aria-expanded="true" aria-controls="collapse2">
-                                                Preterite Tense
-                                            </button>
-                                        </h2>
-                                    </div>
-
-                                    <div id="collapse2" className="collapse" aria-labelledby="heading2"
-                                         data-parent="#accordion2">
-                                        <div className="card-body">
-                                            <p className={"font-italic"}>i &rarr; ie</p>
-                                            <div className={"row"}>
-                                                <div className={"col-6"}>
-                                                    <p>
-                                                        <span className={"text-muted"}>yo</span> trabaje<br/>
-                                                        <span className={"text-muted"}>tu</span> trabajaste<br/>
-                                                        <span className={"text-muted"}>el</span> trabajo
-                                                    </p>
-                                                </div>
-                                                <div className={"col-6"}>
-                                                    <p>
-                                                        <span className={"text-muted"}>nosotros</span> trabajamos<br/>
-                                                        <span className={"text-muted"}>ellos / ustedes</span> trabajaron
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {showText && (<h6 className="card-text text-focus-in">{theCard.translation}</h6>)}
+                    {showText && theCard.example && (
+                        <p className="card-text text-focus-in">{theCard.example}<br/>
+                            <span className={"font-italic"}>{theCard.exampleTranslation}</span>
+                        </p>
+                    )}
+                    {!showText && (<h6 className="card-text">&nbsp;</h6>)}
+                    {extraInfo}
                 </div>
             </div>
         </div>
