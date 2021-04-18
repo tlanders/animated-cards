6bc3981e1804cffadd06aaebbb0e31e32452428b
Troy Landers <tlanders@landersconsulting.biz>
2021-04-17 21:27:26 -0500

    added collapsible verb sections

--- src/FlipCard.js
+++ src/FlipCard.js
@@ -66,6 +66,84 @@ export const FlipCard = (props) => {
                             </p>
                         </div>
                     </div>
+                    <div className={"row"}>
+                        <div className={"col-12"}>
+                            <div className="accordion" id="accordionExample">
+                                <div className="card">
+                                    <div className="card-header" id="headingOne">
+                                        <h2 className="mb-0">
+                                            <button className="btn btn-link btn-block text-center" type="button"
+                                                    data-toggle="collapse" data-target="#collapseOne"
+                                                    aria-expanded="true" aria-controls="collapseOne">
+                                                Imperfect Tense
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseOne" className="collapse" aria-labelledby="headingOne"
+                                         data-parent="#accordionExample">
+                                        <div className="card-body">
+                                            <p className={"font-italic"}>o &rarr; ue</p>
+                                            <div className={"row"}>
+                                                <div className={"col-6"}>
+                                                    <p>
+                                                        <span className={"text-muted"}>yo</span> trabajaba<br/>
+                                                        <span className={"text-muted"}>tu</span> trabajabas<br/>
+                                                        <span className={"text-muted"}>el</span> trabajaba
+                                                    </p>
+                                                </div>
+                                                <div className={"col-6"}>
+                                                    <p>
+                                                        <span className={"text-muted"}>nosotros</span> trabajamos<br/>
+                                                        <span className={"text-muted"}>ellos / ustedes</span> trabajaban
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"row"}>
+                        <div className={"col-12"}>
+                            <div className="accordion" id="accordion2">
+                                <div className="card">
+                                    <div className="card-header" id="heading2">
+                                        <h2 className="mb-0">
+                                            <button className="btn btn-link btn-block text-center" type="button"
+                                                    data-toggle="collapse" data-target="#collapse2"
+                                                    aria-expanded="true" aria-controls="collapse2">
+                                                Preterite Tense
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapse2" className="collapse" aria-labelledby="heading2"
+                                         data-parent="#accordion2">
+                                        <div className="card-body">
+                                            <p className={"font-italic"}>i &rarr; ie</p>
+                                            <div className={"row"}>
+                                                <div className={"col-6"}>
+                                                    <p>
+                                                        <span className={"text-muted"}>yo</span> trabaje<br/>
+                                                        <span className={"text-muted"}>tu</span> trabajaste<br/>
+                                                        <span className={"text-muted"}>el</span> trabajo
+                                                    </p>
+                                                </div>
+                                                <div className={"col-6"}>
+                                                    <p>
+                                                        <span className={"text-muted"}>nosotros</span> trabajamos<br/>
+                                                        <span className={"text-muted"}>ellos / ustedes</span> trabajaron
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
